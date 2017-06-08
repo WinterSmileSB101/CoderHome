20986fcde4770f35c78803346179a39169c39ca6
WinterSmileSB101 <zxb13194889618@outlook.com>
2017-06-08 13:47:14 +0800

    增加返回顶部

--- app/src/main/java/winter/zxb/smilesb101/coderhome/View/Fragments/GanHuo/VideoGanioFragment.java
+++ app/src/main/java/winter/zxb/smilesb101/coderhome/View/Fragments/GanHuo/VideoGanioFragment.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewPropertyAnimatorListener;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -17,6 +19,7 @@ import winter.zxb.smilesb101.coderhome.Presenter.IGanioNotTextFragmentPresenter;
 import winter.zxb.smilesb101.coderhome.R;
 import winter.zxb.smilesb101.coderhome.View.Adapter.VideoRecyclerViewAdapter;
 import winter.zxb.smilesb101.coderhome.View.Interface.INotTextGanioFragmentView;
+import winter.zxb.smilesb101.coderhome.View.Utils.AnimationUtils;
 import winter.zxb.smilesb101.coderhome.View.Utils.StaticUtils;
 import winter.zxb.smilesb101.coderhome.databinding.VideoGanioFragmentLayoutBinding;
 
@@ -34,6 +37,38 @@ public class VideoGanioFragment extends FragmentBase implements INotTextGanioFra
 
 	VideoGanioFragmentLayoutBinding binding;
 	IGanioNotTextFragmentPresenter presenter;
+
+	RecyclerView recyclerView;
+
+	private boolean isAnimatingOut = false;
+
+	ViewPropertyAnimatorListener viewPropertyAnimatorListener = new ViewPropertyAnimatorListener() {
+
+		@Override
+		public void onAnimationStart(View view) {
+			isAnimatingOut = true;
+		}
+
+		@Override
+		public void onAnimationEnd(View view) {
+			isAnimatingOut = false;
+			view.setVisibility(View.GONE);
+		}
+
+		@Override
+		public void onAnimationCancel(View arg0) {
+			isAnimatingOut = false;
+		}
+	};
+
+	View.OnClickListener fab_cilckListener = new View.OnClickListener(){
+		@Override
+		public void onClick(View v){
+			if(recyclerView!=null)
+				recyclerView.smoothScrollToPosition(0);
+		}
+	};
+
 	public static VideoGanioFragment newInstance(){
 		
 		Bundle args = new Bundle();
@@ -56,8 +91,40 @@ public class VideoGanioFragment extends FragmentBase implements INotTextGanioFra
 		binding = DataBindingUtil.inflate(layoutInflater,R.layout.video_ganio_fragment_layout,container,false);
 		rootView = binding.getRoot();
 		rootContext = container.getContext();
-		RecyclerView rv = (RecyclerView)rootView.findViewById(R.id.recyclerView);
-		rv.setLayoutManager(new LinearLayoutManager(rootContext,LinearLayoutManager.VERTICAL,false));
+		final FloatingActionButton fab = (FloatingActionButton)rootView.findViewById(R.id.fab);
+		fab.setOnClickListener(fab_cilckListener);
+		fab.setScaleX(0.0f);
+		fab.setScaleY(0.0f);
+		fab.setAlpha(0.0f);
+		fab.setVisibility(View.GONE);
+		recyclerView = (RecyclerView)rootView.findViewById(R.id.recyclerView);
+		LinearLayoutManager layout = new LinearLayoutManager(this.rootContext,LinearLayoutManager.VERTICAL, false);
+		recyclerView.setLayoutManager(layout);
+		recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener(){
+			@Override
+			public void onScrollStateChanged(RecyclerView recyclerView,int newState){
+				super.onScrollStateChanged(recyclerView,newState);
+
+			}
+
+			@Override
+			public void onScrolled(RecyclerView recyclerView,int dx,int dy){
+				super.onScrolled(recyclerView,dx,dy);
+				if(((LinearLayoutManager)recyclerView.getLayoutManager()).findFirstCompletelyVisibleItemPosition()==0)
+				{
+					if(fab.getVisibility()!=View.GONE && !isAnimatingOut) {
+						AnimationUtils.scaleHide(fab,viewPropertyAnimatorListener);
+					}
+				}
+				else
+				{
+					if(fab.getVisibility()!=View.VISIBLE)
+						AnimationUtils.scaleShow(fab,null);
+				}
+			}
+		});
+//		RecyclerView rv = (RecyclerView)rootView.findViewById(R.id.recyclerView);
+//		rv.setLayoutManager(new LinearLayoutManager(rootContext,LinearLayoutManager.VERTICAL,false));
 
 		presenter = new IGanioNotTextFragmentPresenter(this);
 		presenter.getSearchGanio();
